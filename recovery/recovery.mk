e9ccb2201cc191cf51d64ab00d80f087e7955d4c
wjb <jaybob413@gmail.com>
2013-02-03 11:03:29 -0500

    tf700t: Restore custom recovery image build
    Change-Id: Ic3d0699b4b9a439fc90b01bc6500945cb58264d2

--- recovery/recovery.mk
+++ recovery/recovery.mk
@@ -1,15 +1,15 @@
 TF_BLOBIFIER := $(HOST_OUT_EXECUTABLES)/blobpack_tfp
 
 
-#$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
-#		$(recovery_ramdisk) \
-#		$(TF_BLOBIFIER) \
-#		$(recovery_kernel)
-#	@echo ----- Making recovery image ------
-#	$(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) --output $@.orig
-#	$(TF_BLOBIFIER) $@ SOS $@.orig
-#	@echo ----- Made recovery image -------- $@
-#	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
+$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
+		$(recovery_ramdisk) \
+		$(TF_BLOBIFIER) \
+		$(recovery_kernel)
+	@echo ----- Making recovery image ------
+	$(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) --output $@.orig
+	$(TF_BLOBIFIER) $@ SOS $@.orig
+	@echo ----- Made recovery image -------- $@
+	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
 
 
 $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(TF_BLOBIFIER)
